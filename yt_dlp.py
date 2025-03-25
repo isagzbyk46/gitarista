c0e90411216e31637901918a7e1d48a6109f841d
isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>
2025-03-25 21:51:36 +0300

    Update yt_dlp.py
    Signed-off-by: isagzbyk46 <101513360+isagzbyk46@users.noreply.github.com>

--- yt_dlp.py
+++ yt_dlp.py
@@ -1,8 +1,1 @@
-import yt_dlp
 
-ydl_opts = {
-    'cookies': 'cookies/cookies.txt',  # Repository içindeki çerez dosyasının yolu
-}
-
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['<video_url>'])  # İndirmek istediğiniz video URL'si
